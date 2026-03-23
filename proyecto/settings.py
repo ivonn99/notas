dbad004adb69192cb142b4054686c05df3830d83
ivonn99 <ivonn.99dmh@gmail.com>
2026-03-23 13:02:47 -0600

    Configura estaticos para Render y agrega gunicorn/whitenoise

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -49,6 +49,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -160,6 +161,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
